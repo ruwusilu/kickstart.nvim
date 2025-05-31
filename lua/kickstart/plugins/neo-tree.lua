3afe4a0d0b001313e8278442718c199fdabab096
Cong Ma <congma13@amd.com>
2025-05-31 21:48:34 +0000

    Add clangd, mini.comment

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<F3>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<F3>'] = 'close_window',
         },
       },
     },
