27964c9af6cb8d88fffc89ec2e852f15f10a2cff
Cong Ma <congma13@amd.com>
2025-07-13 21:05:10 -0500

    Modified keymap of search tags

--- init.lua
+++ init.lua
@@ -378,7 +378,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>s]', builtin.tags, { desc = '[S]earch tags' })
+      vim.keymap.set('n', '<leader>st', builtin.tags, { desc = '[S]earch tags' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
